335824c14c7190ce0270f71cbc3eb22fee79ba9d
vipinshar <vipin_sharma03@hotmail.com>
2017-10-22 22:04:59 +0530

    a

--- middlewares/originmiddleware.js
+++ middlewares/originmiddleware.js
@@ -2,6 +2,7 @@ module.exports = function (router) {
     router.use(function (req, res, next) {
         debugger;
         var whitelist = 'blooddonation.cybersipahi.org/'
+        console.log(req.hostname);
         if (req.hostname == whitelist) {
             next();
         } else {
