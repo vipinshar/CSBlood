ceb11562df3685f71682b558aa5b5c86ad63d0b1
vipinshar <vipin_sharma03@hotmail.com>
2017-10-22 22:16:31 +0530

    A

--- middlewares/originmiddleware.js
+++ middlewares/originmiddleware.js
@@ -1,6 +1,9 @@
 module.exports = function (router) {
     router.use(function (req, res, next) {
         debugger;
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
         var whitelist = 'blooddonation.cybersipahi.org';
         console.log(req.hostname+"Viin");
         if (req.hostname == whitelist) {
