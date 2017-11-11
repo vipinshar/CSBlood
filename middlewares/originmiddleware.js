824409992f9e6715b60db5c95041321aa0523e6b
vipinshar <vipin_sharma03@hotmail.com>
2017-11-11 16:14:36 +0530

    host

--- middlewares/originmiddleware.js
+++ middlewares/originmiddleware.js
@@ -14,10 +14,7 @@ module.exports = function (router,app) {
             //to the API (e.g. in case you use sessions)
             res.setHeader('Access-Control-Allow-Credentials', true);
         
-            //Pass to next layer of middleware
-            if (req.headers.host != "blooddonation.cybersipahi.org") {
-                res.send('Invalid');
-            }
+         
             next();
         });
 }
